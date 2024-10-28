8a1e9507813d3d8cf3c4e393d4f9d89db0b09af5
Preethikasekar01 <preethikasekar01@gmail.com>
2024-10-28 12:26:50 +0530

    Add files via upload

---
+++ district.java
@@ -0,0 +1,14 @@
+class district {
+    static double area(){
+        int r = 87;
+        final double pi = 76.0988;
+        double result = pi*r*r;
+        return result;
+    }
+    public static void main(String []args){
+        double x= area ();
+    System.out.println("area is "+x);
+
+
+    }
+}
