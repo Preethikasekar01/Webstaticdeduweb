e42631ee25d1c1c571cdc85e1750740ca230536e
Preethikasekar01 <preethikasekar01@gmail.com>
2024-10-28 12:19:34 +0530

    first commit
    ok

---
+++ Days.java
@@ -0,0 +1,22 @@
+ class if_condition  {
+    public static void main(String[] args) {
+        int a = 35;
+        int b = 20;
+        int c = 54;
+        
+        if (a>b){
+           System.out.println("a is greater then b");
+
+        }
+          else if(a<c) {
+               System.out.println("a is greater then c");
+
+          }
+          else{
+            System.out.println("b is greater then c");
+          }
+          }
+    }
+
+    
+
